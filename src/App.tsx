db4ba949b36a1b46f9aa10bbb6564805cea860cc
Zine-eddineBardoud <zineddinebardoud38@gmail.com>
2025-09-30 10:57:08 +0100

    Implement the NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import NavBar from './components/NavBar'
 
 const App = () => {
     return (
-        <div className='bg-red-500'>App</div>
+        <main>
+            <NavBar />
+        </main>
     )
 }
 
