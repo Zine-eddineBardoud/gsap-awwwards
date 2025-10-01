339a82f2c0d8cd432bcfecc9991486a9f3ecc9b3
Zine-eddineBardoud <zineddinebardoud38@gmail.com>
2025-10-01 13:10:26 +0100

    Add GSAP animations to HeroSection

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
 import NavBar from './components/NavBar'
 import HeroSection from './sections/HeroSection'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
     return (
         <main>
             <NavBar />
             <HeroSection />
+            <div className="h-dvh"></div>
         </main>
     )
 }
