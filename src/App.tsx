4578549c8768e641058cd9efbbfb9e4a09539973
Zine-eddineBardoud <zineddinebardoud38@gmail.com>
2025-10-11 17:45:26 +0100

    Implement Horizontal Scrolling and Scroll Smoother

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
 import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/all';
+import { ScrollSmoother, ScrollTrigger } from 'gsap/all';
 import NavBar from './components/NavBar';
 import HeroSection from './sections/HeroSection';
 import MessageSection from './sections/MessageSection';
 import FlavorSection from './sections/FlavorSection';
+import { useGSAP } from '@gsap/react';
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
+    useGSAP(() => {
+        ScrollSmoother.create({
+            smooth: 1,
+            effects: true,
+        })
+    });
+
     return (
         <main>
             <NavBar />
-            <HeroSection />
-            <MessageSection />
-            <FlavorSection />
-            <div className="h-dvh"></div>
+            <div id="smooth-wrapper">
+                <div id="smooth-content">
+                    <HeroSection />
+                    <MessageSection />
+                    <FlavorSection />
+                    <div className="h-dvh"></div>
+                </div>
+            </div>
         </main>
     )
 }
